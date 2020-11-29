4cef7e15f47580c28a2ce7031e4e8ce4f6b1f0d4
Ivan Averin <i.averin@gmail.com>
2020-11-29 18:29:02 +0300

    added unique email constraint

--- migrations/20201115004419_core_db.js
+++ migrations/20201115004419_core_db.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('users', function (table) {
             table.uuid('user_uuid').primary()
-            table.string('email', 255).notNullable();
+            table.string('email', 255).notNullable().unique("index_email");
             table.string('passHash', 255).notNullable();
         })
 };
